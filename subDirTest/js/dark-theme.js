266b9ff3f82035a2075b502ab6949f088637944a
Jude <judegiordano@gmail.com>
2020-02-14 14:21:07 -0500

    skill sliders

--- subDirTest/js/dark-theme.js
+++ subDirTest/js/dark-theme.js
@@ -6,14 +6,13 @@ $(document).ready(function () {
             $("body").css("background-color", "black");
             $(".bigText").css("color", "white");
             $(".card-content").css("background-color", "#3c3737");
-            $(".content-text-1, .content-text-2").css("color", "lightgrey");
+            $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
             $dark = true
-        }
-        else{
+        } else {
             $("body").css("background-color", "white");
             $(".bigText").css("color", "black");
             $(".card-content").css("background-color", "#f9f9f9");
-            $(".content-text-1, .content-text-2").css("color", "black");
+            $(".content-text-1, .content-text-2, p, .skills-css").css("color", "black");
             $dark = false
         }
     });
---
+++ subDirTest/js/load-sliders.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".skills-html").css("width", "90%")
+    }, 2000);
+    setTimeout(function () {
+        $(".skills-js").css("width", "85%")
+    }, 2300);
+    setTimeout(function () {
+        $(".skills-python").css("width", "80%")
+    }, 2500);
+    setTimeout(function () {
+        $(".skills-cs").css("width", "75%")
+    }, 2800);
+});
