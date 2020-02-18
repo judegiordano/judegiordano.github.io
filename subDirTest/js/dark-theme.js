6e4f753716dd5d9a6ca094f83a47a8ddaa489017
Jude <judegiordano@gmail.com>
2020-02-18 11:49:54 -0500

    consistent naming convention

--- subDirTest/js/dark-theme.js
+++ subDirTest/js/dark-theme.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
     $(".slider-input").click(function () {
         if ($dark === false) {
             $("body").css("background-color", "#292929");
-            $(".bigText").css("color", "white");
+            $(".big-text").css("color", "white");
             $(".card-content").css("background-color", "#3c3737");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
             $dark = true
         } else {
             $("body").css("background-color", "white");
-            $(".bigText").css("color", "black");
+            $(".big-text").css("color", "black");
             $(".card-content").css("background-color", "#f9f9f9");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "black");
             $dark = false
