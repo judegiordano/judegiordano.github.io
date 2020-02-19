fc7d4da9a3d8e7c01135955dc93cb854b810c7ed
Jude <judegiordano@gmail.com>
2020-02-19 09:32:56 -0500

    dark theme for social media logos

--- subDirTest/js/dark-theme.js
+++ subDirTest/js/dark-theme.js
@@ -7,20 +7,23 @@ $(document).ready(function () {
             $(".big-text").css("color", "white");
             $(".card-content").css("background-color", "#3c3737");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
+            $(".mail").html("&#xf003;");
+            $(".facebook").html("&#xf082;");
+            $(".github").html("&#xf09b;");
+            $(".linkedin").html("&#xf08c;");
+            $(".twitter").html("&#xf081;");
             $dark = true
         } else {
             $("body").css("background-color", "white");
             $(".big-text").css("color", "black");
             $(".card-content").css("background-color", "#f9f9f9");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "black");
+            $(".mail").html("&#xf0e0;");
+            $(".facebook").html("&#xf09a;");
+            $(".github").html("&#xf092;");
+            $(".linkedin").html("&#xf0e1;");
+            $(".twitter").html("&#xf099;");
             $dark = false
         }
     });
 });
-// #3c3737
-
-// <i style="font-size:24px" class="fa">&#xf09a;</i> facebook
-// <i style="font-size:24px" class="fa">&#xf09b;</i> git
-// <i style="font-size:24px" class="fa">&#xf08c;</i> linkedin
-// <i style="font-size:24px" class="fa">&#xf081;</i> twitter
-// <i style="font-size:24px" class="fa">&#xf003;</i> envelope
