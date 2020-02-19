3c082782c81155e08e697b429737a82cfd49019c
Jude <judegiordano@gmail.com>
2020-02-19 09:46:20 -0500

    add phone / font logos fade colors now

--- subDirTest/js/dark-theme.js
+++ subDirTest/js/dark-theme.js
@@ -1,4 +1,4 @@
-$dark = false
+var $dark = false
 
 $(document).ready(function () {
     $(".slider-input").click(function () {
@@ -7,22 +7,14 @@ $(document).ready(function () {
             $(".big-text").css("color", "white");
             $(".card-content").css("background-color", "#3c3737");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
-            $(".mail").html("&#xf003;");
-            $(".facebook").html("&#xf082;");
-            $(".github").html("&#xf09b;");
-            $(".linkedin").html("&#xf08c;");
-            $(".twitter").html("&#xf081;");
+            $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "lightgrey");
             $dark = true
         } else {
             $("body").css("background-color", "white");
             $(".big-text").css("color", "black");
             $(".card-content").css("background-color", "#f9f9f9");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "black");
-            $(".mail").html("&#xf0e0;");
-            $(".facebook").html("&#xf09a;");
-            $(".github").html("&#xf092;");
-            $(".linkedin").html("&#xf0e1;");
-            $(".twitter").html("&#xf099;");
+            $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "black");
             $dark = false
         }
     });
