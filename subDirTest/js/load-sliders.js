e02390bd8a7983746bb0de560b92218d61d79ed5
Jude <judegiordano@gmail.com>
2020-02-19 11:17:47 -0500

    cache to save loaded skill bars

--- subDirTest/js/load-sliders.js
+++ subDirTest/js/load-sliders.js
@@ -1,32 +1,30 @@
-// $(document).ready(function () {
-//     setTimeout(function () {
-//         $(".skills-html").css("width", "90%")
-//     }, 1000);
-//     setTimeout(function () {
-//         $(".skills-js").css("width", "85%")
-//     }, 1200);
-//     setTimeout(function () {
-//         $(".skills-python").css("width", "80%")
-//     }, 1400);
-//     setTimeout(function () {
-//         $(".skills-cs").css("width", "75%")
-//     }, 1600);
-// });
+var $triggered_times = 0;
 
 $(document).ready(function ($) {
 
-    $triggered_times = 0;
+    var isTriggered = localStorage.getItem("triggered");
+
+    if (isTriggered == "true") {
+        $(".skills-html").css("width", "90%").css("transition", "all ease-in-out 0s");
+        $(".skills-js").css("width", "85%").css("transition", "all ease-in-out 0s");
+        $(".skills-python").css("width", "80%").css("transition", "all ease-in-out 0s");
+        $(".skills-cs").css("width", "75%").css("transition", "all ease-in-out 0s");
+        console.log(`skill bars loaded: ${isTriggered}`);
+        $triggered_times = 1;
+    }
 
     $(window).on('scroll', function () {
+
         var y_scroll_pos = window.pageYOffset;
         var scroll_pos_test = 150;
 
         if (y_scroll_pos > scroll_pos_test && $triggered_times == 0) {
-            $(".skills-html").css("width", "90%").css("transition", "all ease-in-out 1.5s")
-            $(".skills-js").css("width", "85%").css("transition", "all ease-in-out 1.7s")
-            $(".skills-python").css("width", "80%").css("transition", "all ease-in-out 1.9s")
-            $(".skills-cs").css("width", "75%").css("transition", "all ease-in-out 2.1s")
+            $(".skills-html").css("width", "90%").css("transition", "all ease-in-out 1.5s");
+            $(".skills-js").css("width", "85%").css("transition", "all ease-in-out 1.7s");
+            $(".skills-python").css("width", "80%").css("transition", "all ease-in-out 1.9s");
+            $(".skills-cs").css("width", "75%").css("transition", "all ease-in-out 2.1s");
             $triggered_times = 1;
+            localStorage.setItem("triggered", "true");
         }
     });
 })
