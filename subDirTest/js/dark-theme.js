51937efc1d23ae530e563cdc2fdf7651b05b44a5
Jude <judegiordano@gmail.com>
2020-02-19 12:38:03 -0500

    documenting js / shrink profile for media query

--- subDirTest/js/dark-theme.js
+++ subDirTest/js/dark-theme.js
@@ -1,18 +1,25 @@
+// instantiate global dark variable
 var $dark;
 
+// on document ready
 $(document).ready(function () {
-
+    // use local storage to check dark theme value
     var isDark = localStorage.getItem("darkTheme");
 
+    // if dark theme is enabled
     if (isDark == "true") {
+        // set global dark to 'true'
         $dark = true
+        // change theme of page
         $("body").css("background-color", "#292929");
         $(".big-text").css("color", "white");
         $(".card-content").css("background-color", "#3c3737");
         $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
         $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "lightgrey");
     } else {
+        // set global dark to 'false'
         $dark = false
+        // change theme of page
         $("body").css("background-color", "white");
         $(".big-text").css("color", "black");
         $(".card-content").css("background-color", "#f9f9f9");
@@ -20,22 +27,30 @@ $(document).ready(function () {
         $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "black");
     }
 
+    // when the slider input is clicked
     $(".slider-input").click(function () {
+        // check global dark value
         if ($dark === false) {
+            // inverse page theme
             $("body").css("background-color", "#292929").css("transition", "none");
             $(".big-text").css("color", "white");
             $(".card-content").css("background-color", "#3c3737");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "lightgrey");
             $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "lightgrey");
+            // set local storage theme value
             localStorage.setItem("darkTheme", "true");
+            // set global dark
             $dark = true;
         } else {
+            // inverse page theme
             $("body").css("background-color", "white");
             $(".big-text").css("color", "black");
             $(".card-content").css("background-color", "#f9f9f9");
             $(".content-text-1, .content-text-2, p, .skills-css").css("color", "black");
             $(".mail, .facebook, .github, .linkedin, .twitter, .phone").css("color", "black");
+            // set local storage theme value
             localStorage.setItem("darkTheme", "false");
+            // set global dark
             $dark = false;
         }
     });
--- subDirTest/js/load-sliders.js
+++ subDirTest/js/load-sliders.js
@@ -1,29 +1,42 @@
+// instantiate global vkaue for if skill bars are triggered
 var $triggered_times = 0;
 
+// on page ready
 $(document).ready(function ($) {
 
+    // get local storage item of 'triggered'
     var isTriggered = localStorage.getItem("triggered");
 
+    // if 'triggered' is true
     if (isTriggered == "true") {
-        $(".skills-html").css("width", "90%").css("transition", "all ease-in-out 0s");
-        $(".skills-js").css("width", "85%").css("transition", "all ease-in-out 0s");
-        $(".skills-python").css("width", "80%").css("transition", "all ease-in-out 0s");
-        $(".skills-cs").css("width", "75%").css("transition", "all ease-in-out 0s");
+        // have skill bars fully loaded with no transition speed
+        $(".skills-html").css("width", "90%").css("transition", "none");
+        $(".skills-js").css("width", "85%").css("transition", "none");
+        $(".skills-python").css("width", "80%").css("transition", "none");
+        $(".skills-cs").css("width", "75%").css("transition", "none");
+        // log value of 'triggered'
         console.log(`skill bars loaded: ${isTriggered}`);
+        // set global triggered value to 1
         $triggered_times = 1;
     }
 
+    // when user scrolls onto element
     $(window).on('scroll', function () {
 
+        // offset page window 
         var y_scroll_pos = window.pageYOffset;
         var scroll_pos_test = 150;
 
+        // if user scrolls onto determined offset (only if not triggered)
         if (y_scroll_pos > scroll_pos_test && $triggered_times == 0) {
+            // slowly load skill bars width
             $(".skills-html").css("width", "90%").css("transition", "all ease-in-out 1.5s");
             $(".skills-js").css("width", "85%").css("transition", "all ease-in-out 1.7s");
             $(".skills-python").css("width", "80%").css("transition", "all ease-in-out 1.9s");
             $(".skills-cs").css("width", "75%").css("transition", "all ease-in-out 2.1s");
+            // set trigered times to 1
             $triggered_times = 1;
+            // local storage set to true to not trigger again
             localStorage.setItem("triggered", "true");
         }
     });
