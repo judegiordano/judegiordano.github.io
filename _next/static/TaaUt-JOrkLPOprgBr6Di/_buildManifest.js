05dc20f561f9e3939ad6d084ba34d23765c99894
JudeBoy <judegiordano@gmail.com>
2021-09-26 13:44:22 -0400

    Updates

---
+++ _next/static/TaaUt-JOrkLPOprgBr6Di/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/371-490ccf6163b11ae259e9.js","static/chunks/pages/index-4f578012d4c363ed84a0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/qx6wQkXFp-yl-ucYzAsjy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/778-53e4223d993649931fa2.js","static/chunks/pages/index-a1e38da5c6ba26cd7179.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
