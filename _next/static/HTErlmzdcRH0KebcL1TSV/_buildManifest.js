b590e456b1901f62a32fab4e0c58c51c6f30c844
JudeBoy <judegiordano@gmail.com>
2021-09-26 13:02:54 -0400

    Updates

---
+++ _next/static/HTErlmzdcRH0KebcL1TSV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/778-53e4223d993649931fa2.js","static/chunks/pages/index-d7a4dc4b555a72e00aec.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
